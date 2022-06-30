765635474fb953640cca1d010a369275bbe87675
Garcia Jhon Lucas <garciajhonlucas@gmail.com>
2022-06-29 20:37:01 -0500

    start rick & morty project

---
+++ src/components/characters.js
@@ -0,0 +1,41 @@
+export default function Characters(props) {
+    const {characters, setCharacters } = props;
+
+    const resetCharacter = () => {
+        setCharacters(null);
+    }
+
+    return (
+        <div className="characters">
+            <h1>Characters</h1>
+            <span className="back-home" onClick={resetCharacter}>Back Home</span>
+            <div className="container-characters">
+                {characters.map((character, index) => (
+                    <div className="character-container" key={index}>
+                        <div>
+                            <img src={character.image} alt={character.name} />
+                        </div>
+                        <div>
+                            <h3>{character.name}</h3>
+                            <h6>
+                                {character.status === 'Alive' ? (
+                                <> <span className="alive"></span> Alive</>
+                                ) : (
+                                <> <span className="dead"></span> Dead</>
+                                )}</h6>
+                            <p>
+                                <span className="text-grey">Episodes: </span>
+                                <span>{character.episode.length}</span>
+                            </p>
+                            <p>
+                                <span className="text-grey">Species: </span>
+                                <span>{character.species}</span>
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <span className="back-home" onClick={resetCharacter}>Back Home</span>
+        </div>
+    );
+}
